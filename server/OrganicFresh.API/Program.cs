3d729e83a03e7ea29fc01cbba38c3471cb0e7e43
modestsp <csperichon@gmail.com>
2023-10-10 15:57:30 -0300

    Added connection string and Identity service

--- server/OrganicFresh.API/Program.cs
+++ server/OrganicFresh.API/Program.cs
@@ -1,6 +1,44 @@
+using System.Text;
+using Ecommerce.Entities.DbSet;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using OrganicFresh.API.DataService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var configuration = builder.Configuration;
+var connectionString = configuration.GetConnectionString("PostgreSQLConnection");
+
+builder.Services.AddDbContext<MyDbContext>(options => options.UseNpgsql(connectionString));
+
+// Identity
+builder.Services
+    .AddIdentityCore<User>()
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<MyDbContext>();
+
+
+// Jwt Config
+builder.Services
+    .AddHttpContextAccessor()
+    .AddAuthorization()
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["JWT:Issuer"],
+            ValidAudience = builder.Configuration["JWT:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"]))
+        };
+    });
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
