a8cac916feb753365644cc4ca988d1afc4effb78
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-10-11 17:22:41 -0300

    Se agregaron importaciones

--- client/OrganicFresh/src/app/modules/shared/shared.module.ts
+++ client/OrganicFresh/src/app/modules/shared/shared.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatButtonModule} from '@angular/material/button'
+import {MatButtonModule} from '@angular/material/button';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 
 
@@ -10,7 +13,10 @@ import {MatButtonModule} from '@angular/material/button'
     CommonModule
   ],
   exports:[
-    MatButtonModule
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ]
 })
 export class SharedModule { }
