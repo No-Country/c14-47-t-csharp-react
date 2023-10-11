1dd1756d746602e6fe7557e4788b719382b3037a
modestsp <csperichon@gmail.com>
2023-10-11 18:05:38 -0300

    Updated Fields

--- server/OrganicFreshAPI/OrganicFreshAPI/Entities/DbSet/User.cs
+++ server/OrganicFreshAPI/OrganicFreshAPI/Entities/DbSet/User.cs
@@ -4,9 +4,9 @@ namespace OrganicFreshAPI.Entities.DbSet;
 
 public class User : IdentityUser
 {
-    public string FirstName { get; set; } = string.Empty;
-    public string LastName { get; set; } = string.Empty;
-    public DateTime CreatedAt { get; set; } = DateTime.Now;
-    public DateTime ModifiedAt { get; set; } = DateTime.Now;
-    public DateTime DeletedAt { get; set; } = DateTime.Now;
+    public string Name { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime ModifiedAt { get; set; } = DateTime.UtcNow;
+    public DateTime DeletedAt { get; set; } = DateTime.UtcNow;
 }
+
