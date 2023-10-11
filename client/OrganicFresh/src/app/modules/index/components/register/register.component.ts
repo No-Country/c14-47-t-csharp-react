d79b07fc13affc52599934e88b2f500ee69f921a
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-10-11 00:04:00 -0300

    Se terminó el diseño del registro para mobile

--- client/OrganicFresh/src/app/modules/index/components/register/register.component.ts
+++ client/OrganicFresh/src/app/modules/index/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+
+  constructor(fb:FormBuilder){
+
+    this.form = fb.group({
+      username:['',[Validators.required]],
+      email: ['',[Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+
+  }
+
+
+  form:FormGroup;
 
 }
