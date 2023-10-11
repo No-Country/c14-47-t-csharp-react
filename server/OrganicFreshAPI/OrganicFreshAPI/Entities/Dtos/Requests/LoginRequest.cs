436589b79d7de9065d1049b4c03e5947489af48b
modestsp <csperichon@gmail.com>
2023-10-11 12:32:07 -0300

    Created Login and Register Requests

---
+++ server/OrganicFreshAPI/OrganicFreshAPI/Entities/Dtos/Requests/LoginRequest.cs
@@ -0,0 +1,7 @@
+namespace OrganicFreshAPI.Entities.Dtos.Requests;
+
+public class LoginRequest
+{
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
