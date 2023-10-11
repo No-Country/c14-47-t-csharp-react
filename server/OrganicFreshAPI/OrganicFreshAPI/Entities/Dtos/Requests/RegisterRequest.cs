1aa3fc64fe49e9d5cd26754a6b30ab84dacbafea
modestsp <csperichon@gmail.com>
2023-10-11 18:05:48 -0300

    Updated fields

--- server/OrganicFreshAPI/OrganicFreshAPI/Entities/Dtos/Requests/RegisterRequest.cs
+++ server/OrganicFreshAPI/OrganicFreshAPI/Entities/Dtos/Requests/RegisterRequest.cs
@@ -2,8 +2,7 @@ namespace OrganicFreshAPI.Entities.Dtos.Requests;
 
 public class RegisterRequest
 {
-    public string FirstName { get; set; } = string.Empty;
-    public string LastName { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
 }
