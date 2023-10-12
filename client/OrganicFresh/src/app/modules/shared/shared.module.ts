f98be55254c9220e6020848dcf5d68cc74cb1ac9
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-10-12 13:04:28 -0300

    Creación del alertRegistertComponent

--- client/OrganicFresh/src/app/modules/shared/shared.module.ts
+++ client/OrganicFresh/src/app/modules/shared/shared.module.ts
@@ -4,19 +4,27 @@ import {MatButtonModule} from '@angular/material/button';
 import {ReactiveFormsModule} from '@angular/forms';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
+import { AlertRegisterComponent } from './components/alert-register/alert-register.component';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatIconModule} from '@angular/material/icon';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AlertRegisterComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
   ],
   exports:[
     MatButtonModule,
     ReactiveFormsModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatDialogModule,
+    MatIconModule
   ]
 })
 export class SharedModule { }
