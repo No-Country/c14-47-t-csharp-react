3443701ecc4090259d8bcca6aa22398979172bc1
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-10-12 10:51:46 -0300

    Se creó el componente login y se configuró su routing

--- client/OrganicFresh/src/app/modules/index/index-routing.module.ts
+++ client/OrganicFresh/src/app/modules/index/index-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './index.component';
 import { IndexPageComponent } from './components/index-page/index-page.component';
 import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [{
   path:'', component:IndexComponent, 
@@ -12,6 +13,9 @@ const routes: Routes = [{
         },
         {
           path:'register', component:RegisterComponent
+        },
+        {
+          path:'login', component:LoginComponent
         }
       ],
 }];
--- client/OrganicFresh/src/app/modules/index/index.module.ts
+++ client/OrganicFresh/src/app/modules/index/index.module.ts
@@ -6,13 +6,15 @@ import { IndexComponent } from './index.component';
 import { IndexPageComponent } from './components/index-page/index-page.component';
 import { SharedModule } from '../shared/shared.module';
 import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 @NgModule({
   declarations: [
     IndexComponent,
     IndexPageComponent,
-    RegisterComponent
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,
