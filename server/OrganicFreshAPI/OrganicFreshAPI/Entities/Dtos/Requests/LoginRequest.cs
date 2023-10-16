53951c221646a0a7039a034ed93502f6c1f44108
modestsp <csperichon@gmail.com>
2023-10-16 16:24:56 -0300

    Removed

--- server/OrganicFreshAPI/OrganicFreshAPI/Entities/Dtos/Requests/LoginRequest.cs
+++
@@ -1,7 +0,0 @@
-namespace OrganicFreshAPI.Entities.Dtos.Requests;
-
-public class LoginRequest
-{
-    public string Email { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
-}
