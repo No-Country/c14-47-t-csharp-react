f03dd5cc9edd9352d0e3be17876159a2babe400e
modestsp <csperichon@gmail.com>
2023-10-18 14:48:42 -0300

    Updated Image field

--- server/OrganicFreshAPI/OrganicFreshAPI/Entities/DbSet/Product.cs
+++ server/OrganicFreshAPI/OrganicFreshAPI/Entities/DbSet/Product.cs
@@ -6,8 +6,8 @@ public class Product
     public string Name { get; set; } = string.Empty;
     public decimal Price { get; set; }
     public int CategoryId { get; set; }
-    public bool Active { get; set; }
-    public string imageUrl { get; set; } = string.Empty;
+    public bool? Active { get; set; } = true;
+    public string ImageUrl { get; set; } = string.Empty;
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime ModifiedAt { get; set; } = DateTime.UtcNow;
     public DateTime DeletedAt { get; set; } = DateTime.UtcNow;
