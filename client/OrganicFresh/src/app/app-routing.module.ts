afdce7d399cb2ff81ccb86df044ae1d84118f995
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-10-17 23:00:28 -0300

    Se creó el guard para proteger el módulo del admin y se lo implementó

--- client/OrganicFresh/src/app/app-routing.module.ts
+++ client/OrganicFresh/src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { adminGuard } from './guards/admin.guard';
 
 const routes: Routes = [
   {
     path:'index', loadChildren: ()=> import('./modules/index/index.module').then(m => m.IndexModule)
   },
   {
-    path:'admin', loadChildren:()=> import('./modules/admin/admin.module').then(m => m.AdminModule)
+    path:'admin', loadChildren:()=> import('./modules/admin/admin.module').then(m => m.AdminModule), canActivate:[adminGuard]
   },
   {
     path:'**',redirectTo:'index'
