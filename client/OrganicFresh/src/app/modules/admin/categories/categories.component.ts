20c3f7c1d4b2b46ea499094aee37b7febedf3150
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-10-18 14:12:40 -0300

    Se desarrollo la lógica y diseño del componente categorias

--- client/OrganicFresh/src/app/modules/admin/categories/categories.component.ts
+++ client/OrganicFresh/src/app/modules/admin/categories/categories.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/interfaces/category';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.scss']
 })
-export class CategoriesComponent {
+export class CategoriesComponent implements OnInit{
+
+
+  listCategories:Category[]=[];
+
+  constructor(private categoryService:CategoryService){}
+  ngOnInit(): void {
+
+    this.categoryService.getAll().subscribe({
+      next:(res)=>{
+        this.listCategories = res;
+      }
+    });
+  }
+
+
 
 }
