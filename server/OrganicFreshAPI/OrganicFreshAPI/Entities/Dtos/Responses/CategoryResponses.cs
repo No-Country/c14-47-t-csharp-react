380e566f46bcd9282fd96f7c5801ecf925cfa8d1
modestsp <csperichon@gmail.com>
2023-10-19 12:28:18 -0300

    Added GetProductsFromCategory

--- server/OrganicFreshAPI/OrganicFreshAPI/Entities/Dtos/Responses/CategoryResponses.cs
+++ server/OrganicFreshAPI/OrganicFreshAPI/Entities/Dtos/Responses/CategoryResponses.cs
@@ -3,5 +3,6 @@ using OrganicFreshAPI.Entities.DbSet;
 namespace OrganicFreshAPI.Entities.Dtos.Responses;
 
 public record GetCategoriesResponse(List<Category> categories);
+public record GetProductsFromCategoryResponse(Category category, List<Product> products);
 public record CreateCategoryResponse(Category category);
 public record UpdateCategoryResponse(Category updatedCategory);
