f02d4da97126f2eca4626608a388942564d1d806
modestsp <csperichon@gmail.com>
2023-10-19 12:24:59 -0300

    Updated Migrations

--- server/OrganicFreshAPI/OrganicFreshAPI/Migrations/20231018235230_Added PublicId to Product.Designer.cs
+++ server/OrganicFreshAPI/OrganicFreshAPI/Migrations/20231018235230_Added PublicId to Product.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OrganicFreshAPI.DataService.Data;
 
@@ -11,9 +12,11 @@ using OrganicFreshAPI.DataService.Data;
 namespace OrganicFreshAPI.Migrations
 {
     [DbContext(typeof(MyDbContext))]
-    partial class MyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231018235230_Added PublicId to Product")]
+    partial class AddedPublicIdtoProduct
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
