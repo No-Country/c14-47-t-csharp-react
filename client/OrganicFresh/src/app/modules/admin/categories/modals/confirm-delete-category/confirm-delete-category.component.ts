9a4802b9a8a89573e54f2e05d1a8bbaab0d1dd26
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-10-18 22:02:31 -0300

    Se implementó la funcionalidad para eliminar una categoria

--- client/OrganicFresh/src/app/modules/admin/categories/modals/confirm-delete-category/confirm-delete-category.component.ts
+++ client/OrganicFresh/src/app/modules/admin/categories/modals/confirm-delete-category/confirm-delete-category.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-confirm-delete-category',
@@ -8,11 +9,22 @@ import { MatDialogRef } from '@angular/material/dialog';
 })
 export class ConfirmDeleteCategoryComponent {
 
-  constructor(private dialogRef:MatDialogRef<ConfirmDeleteCategoryComponent>){}
+  constructor(private dialogRef:MatDialogRef<ConfirmDeleteCategoryComponent>, private categoryService:CategoryService,@Inject(MAT_DIALOG_DATA)private idCategory:number){}
 
 
   close():void{
     this.dialogRef.close();
+  }
+
+  delete():void{
+    this.categoryService.delete(this.idCategory).subscribe({
+      next:()=>{
+        this.dialogRef.close(true);
+      },
+      error:()=>{
+
+      }
+    });
   }
 
 }
