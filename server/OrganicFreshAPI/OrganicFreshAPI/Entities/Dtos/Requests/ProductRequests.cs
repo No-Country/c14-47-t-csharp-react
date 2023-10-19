600749749fba033138f16b99791564bd097d1a3a
modestsp <csperichon@gmail.com>
2023-10-19 18:13:49 -0300

    Added WeightUnit

--- server/OrganicFreshAPI/OrganicFreshAPI/Entities/Dtos/Requests/ProductRequests.cs
+++ server/OrganicFreshAPI/OrganicFreshAPI/Entities/Dtos/Requests/ProductRequests.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using OrganicFreshAPI.Entities.DbSet;
 
 namespace OrganicFreshAPI.Entities.Dtos.Requests;
 
@@ -13,6 +14,8 @@ public class CreateProductRequest
     public int CategoryId { get; set; }
     public bool? Active = true;
     public IFormFile? Image { get; set; }
+    public string? WeightUnit { get; set; }
+    public int? Stock { get; set; }
 }
 
 
@@ -22,6 +25,8 @@ public record UpdateProductRequest(
     decimal? price,
     bool? active,
     IFormFile? image,
-    int? categoryId
+    int? categoryId,
+    string? weightUnit,
+    int? stock
 );
 
