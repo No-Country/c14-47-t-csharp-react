0e15397c19c81350af5561edf8a0cea505aa2d44
modestsp <csperichon@gmail.com>
2023-10-22 20:55:09 -0300

    Removed data annotations

--- server/OrganicFreshAPI/OrganicFreshAPI/Entities/Dtos/Requests/AuthRequests.cs
+++ server/OrganicFreshAPI/OrganicFreshAPI/Entities/Dtos/Requests/AuthRequests.cs
@@ -10,14 +10,8 @@ public record LoginRequest(
 
 public record RegisterRequest
 {
-    [Required]
     public string Name { get; set; } = string.Empty;
-    [Required]
-    [EmailAddress(ErrorMessage = Consts.EmailValidationError)]
     public string Email { get; set; } = string.Empty;
-    [Required]
-    [MinLength(Consts.PasswordMinLength, ErrorMessage = Consts.PasswordMinLengthValidationError)]
-    [RegularExpression(Consts.PasswordRegex, ErrorMessage = Consts.PasswordValidationError)]
     public string Password { get; set; } = string.Empty;
 }
 
--- server/OrganicFreshAPI/OrganicFreshAPI/Entities/Dtos/Requests/ProductRequests.cs
+++ server/OrganicFreshAPI/OrganicFreshAPI/Entities/Dtos/Requests/ProductRequests.cs
@@ -5,12 +5,8 @@ namespace OrganicFreshAPI.Entities.Dtos.Requests;
 
 public class CreateProductRequest
 {
-    [Required(ErrorMessage = "The name is required")]
     public string Name { get; set; } = string.Empty;
-    [Required(ErrorMessage = "The price is required")]
     public decimal Price { get; set; }
-
-    [Required(ErrorMessage = "The category id is required")]
     public int CategoryId { get; set; }
     public bool? Active = true;
     public IFormFile? Image { get; set; }
