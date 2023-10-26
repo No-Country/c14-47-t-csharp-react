7fb43ff373d650a4d72837f5e94b2b4a482aab9c
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-10-26 12:11:07 -0300

    Se desarrolló la lógica para mostrar/ocultar el carrito

--- client/OrganicFresh/src/app/modules/index/components/index-page/index-page.component.ts
+++ client/OrganicFresh/src/app/modules/index/components/index-page/index-page.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { Observable, take } from 'rxjs';
+import { CartItem } from 'src/app/interfaces/cartItem';
 import { Category } from 'src/app/interfaces/category';
 import { Credential } from 'src/app/interfaces/credential';
 import { Product } from 'src/app/interfaces/product';
 import { AuthService } from 'src/app/services/auth.service';
+import { CartService } from 'src/app/services/cart.service';
 import { CategoryService } from 'src/app/services/category.service';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -17,8 +20,9 @@ export class IndexPageComponent implements OnInit{
 
   credential$:Observable<Credential|null>;
 
-  constructor(private authService:AuthService, private productService:ProductService, private categoryService:CategoryService){
+  constructor(private authService:AuthService, private productService:ProductService, private categoryService:CategoryService, private router:Router, private cartService:CartService){
     this.credential$ = authService.getCredential;
+    this.showShoppingCart$ = cartService.show;
   }
   ngOnInit(): void {
     this.getCategories();
@@ -32,6 +36,7 @@ export class IndexPageComponent implements OnInit{
   listProducts:Product[] = [];
   listCategories:Category[] = [];
   idSelectedCategory:number | null = null;
+  showShoppingCart$:Observable<boolean>;
 
   getCategories():void{
     this.categoryService.getAll().subscribe({
@@ -54,6 +59,33 @@ export class IndexPageComponent implements OnInit{
   filter(idCategory:number):void{
     this.idSelectedCategory = idCategory;
     this.listProducts = this.listAllProducts.filter( product => product.categoryId === this.idSelectedCategory);
+  }
+
+  addToCart(product:Product):void{
+    this.credential$.pipe(take(1)).subscribe({
+      next:(res)=>{
+        if(res!==null){
+          if(res.isAdmin === false){
+            const cartItem:CartItem = {
+              product:product,
+              productId: product.id!,
+              quantity:1,
+              subtotal:product.price*1
+            }
+            this.cartService.addItem(cartItem);
+            this.cartService.showCart();
+
+          }
+        }
+        else{
+          this.router.navigate(['index/login']);
+        }
+      }
+    })
+  }
+
+  showCart():void{
+    this.cartService.showCart();
   }
 
 }
